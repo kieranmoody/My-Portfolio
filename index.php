5283e026862cd2b566c35663b7cf72409cc71c6d
Kieran Moody <kieranmoody127@gmail.com>
2026-03-13 10:40:06 +0000

    email sending now works

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
 //Database Connecting//
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Mailtrap\Helper\ResponseHelper;
+use Mailtrap\MailtrapClient;
+use Mailtrap\Mime\MailtrapEmail;
+use Symfony\Component\Mime\Address;
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
@@ -15,6 +20,13 @@ $port = $_ENV['DB_PORT'];
 $db   = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
+
+$apiKey = $_ENV['MAILTRAP_API_KEY'];
+$personalEmail = $_ENV['PERSONAL_EMAIL'];
+
+$mailtrap = MailtrapClient::initSendingEmails(
+    apiKey: $apiKey,
+);
 
 try {
     $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
@@ -98,6 +110,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ':message' => $message,
         ]);
 
+        $email = (new MailtrapEmail())
+            ->from(new Address('hello@demomailtrap.co', $firstName .' '. $lastName))
+            ->to(new Address($personalEmail))
+            ->subject('From Your Portfolio')
+            ->text (
+                "The subject of the message - " . $subject . "\n" .
+                $message . "\n" .
+                "Message came with the email-address - " . $email
+            )
+            ->category('Integration Test')
+        ;
+
+        $response = $mailtrap->send($email);
+
+        var_dump(ResponseHelper::toArray($response));
 
         header("Location: index.php?success=1");
         exit;
